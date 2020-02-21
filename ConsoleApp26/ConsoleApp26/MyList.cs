574736eb6c980dacf5845f4e5b50135281e9fba4
moudroff <moudroff>
2020-02-21 13:06:18 +0300

    MyList1

---
+++ ConsoleApp26/ConsoleApp26/MyList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    public class Elem
+    {
+        public int Info { get; set; }
+        public Elem Next { get; set; }
+    }
+
+    public class MyList
+    {
+        public Elem Head {get; set; }
+
+        public int Length
+        {
+            get
+            {
+                //TODO
+                return 0;
+            }
+        }
+
+        public void AddFirst(int x)
+        {
+            Elem elem = new Elem();
+            elem.Info = x;
+            elem.Next = Head;
+            Head = elem;
+        }
+
+        public void AddLast(int x)
+        {
+            if(Head == null)
+            {
+                Head = new Elem() { Info = x };
+                return;
+            }
+
+            var elem = Head;
+            while (elem.Next != null)
+            {
+                elem = elem.Next;
+            }
+            Elem newEl = new Elem() { Info = x };
+            elem.Next = newEl;
+
+        }
+
+        public void AddAfterK(int k, int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteIndex(int k)
+        {
+            if (Head == null)
+                throw new IndexOutOfRangeException($"в списке отсутствует {k} элемент");
+            if( k == 0 )
+            {
+                Head = Head.Next;
+                return;
+            }
+            var elem = Head;
+            for(int i = 0; i < k-1; i++)
+            {
+                if (elem == null)
+                    throw new IndexOutOfRangeException($"в списке отсутствует {k} элемент");
+                elem = elem.Next;
+
+            }
+            if (elem.Next == null)
+                throw new IndexOutOfRangeException($"в списке отсутствует {k} элемент");
+            elem.Next = elem.Next.Next;
+
+        }
+
+        public void DeleteValue(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            var elem = Head;
+            while(elem != null)
+            {
+                sb.Append($"{elem.Info} -> ");
+                elem = elem.Next;
+            }
+            sb.Append("null");
+            return sb.ToString();
+        }
+
+
+
+    }
+}
--- ConsoleApp26/ConsoleApp26/Program.cs
+++ ConsoleApp26/ConsoleApp26/Program.cs
@@ -7,120 +7,6 @@ using System.Threading.Tasks;
 namespace ConsoleApp26
 {
 
-    public class Elem
-    {
-        public int Info { get; set; }
-        public Elem Next { get; set; }
-
-    }
-
-    public class MyList
-    {
-        public Elem Head { get; set; }
-
-        public int Length
-        {
-            get
-            {
-                //TODO
-                return 0;
-            }
-        }
-
-        public void AddFirst(int x)
-        {
-            var newElem = new Elem();
-            newElem.Info = x;
-            newElem.Next = Head;
-        
-            Head = newElem;
-        }
-
-        public void AddLast(int x)
-        {
-            if (Head == null)
-                Head = new Elem() { Info = x };
-            else
-            {
-                var el = Head;
-                while (el.Next != null)
-                    el = el.Next;
-
-                var newElem = new Elem() { Info = x };
-                el.Next = newElem;
-            }
-        }
-
-
-
-
-        public void AddAfterIndex(int ind, int x)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteValue(int x)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool IsSorted()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public void DeleteIndex(int k)
-        {
-            if (Head == null)
-                throw new ArgumentOutOfRangeException();
-
-            if (k==0)
-            {
-                Head = Head.Next;
-                return;
-            }
-
-            var el = Head;
-            for(int i = 0; i < k-1; i++)
-            {
-                if(el.Next == null)
-                    throw new ArgumentOutOfRangeException();
-                el = el.Next;
-            }
-
-            if (el.Next == null)
-                throw new ArgumentOutOfRangeException();
-            el.Next = el.Next.Next;
-
-        }
-
-
-        
-
-
-
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            var el = Head;
-            while (el != null)
-            {
-                sb.Append($"{el.Info} -> ");
-                el = el.Next;
-            }
-            sb.Append("null");
-            return sb.ToString();
-        }
-
-
-
-      
-        
-
-    }
-
-
     
 
 
@@ -129,18 +15,19 @@ namespace ConsoleApp26
     {
         static void Main(string[] args)
         {
+
             MyList lst = new MyList();
 
-            for (int i = 0; i < 10; i++)
+            Console.WriteLine(lst.Length);
+
+            for (int i = 0; i < 11; i++)
                 lst.AddLast(i);
 
             Console.WriteLine(lst);
 
+            lst.DeleteIndex(5);
 
             Console.WriteLine(lst);
-
-
-
 
         }
     }
