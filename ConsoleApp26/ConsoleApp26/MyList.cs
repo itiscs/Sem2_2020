30fc2088a432a9ae5ca3f4baea5b72e4b0c22547
moudroff <moudroff>
2020-02-28 15:19:47 +0300

    Generics

--- ConsoleApp26/ConsoleApp26/MyList.cs
+++ ConsoleApp26/ConsoleApp26/MyList.cs
@@ -6,6 +6,120 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp26
 {
+    public class Elem<T> where T:IComparable 
+    {
+        public T Info { get; set; }
+        public Elem<T> Next { get; set; }
+    }
+
+    
+    public class MyList<T> where T:IComparable
+    {
+        public Elem<T> Head { get; set; }
+
+        public int Length
+        {
+            get
+            {
+                //TODO
+                return 0;
+            }
+        }
+
+        public void AddFirst(T x)
+        {
+            Elem<T> elem = new Elem<T>();
+            elem.Info = x;
+            elem.Next = Head;
+            Head = elem;
+        }
+
+
+        public void AddLast(T x)
+        {
+            if (Head == null)
+            {
+                Head = new Elem<T>() { Info = x };
+                return;
+            }
+
+            var elem = Head;
+            while (elem.Next != null)
+            {
+                elem = elem.Next;
+            }
+            Elem<T> newEl = new Elem<T>() { Info = x };
+            elem.Next = newEl;
+
+        }
+
+        public void AddAfterK(int k, int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteIndex(int k)
+        {
+            if (Head == null)
+                throw new IndexOutOfRangeException($"в списке отсутствует {k} элемент");
+            if (k == 0)
+            {
+                Head = Head.Next;
+                return;
+            }
+            var elem = Head;
+            for (int i = 0; i < k - 1; i++)
+            {
+                if (elem == null)
+                    throw new IndexOutOfRangeException($"в списке отсутствует {k} элемент");
+                elem = elem.Next;
+
+            }
+            if (elem.Next == null)
+                throw new IndexOutOfRangeException($"в списке отсутствует {k} элемент");
+            elem.Next = elem.Next.Next;
+
+        }
+
+        public void DeleteValue(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public bool IsSorted()
+        {
+            var el = Head;
+            while(el !=null && el.Next != null)
+            {
+                if (el.Info.CompareTo(el.Next.Info) > 0)
+                    return false;
+                el = el.Next;
+
+            }
+            return true;       
+        }
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            var elem = Head;
+            while (elem != null)
+            {
+                sb.Append($"{elem.Info} -> ");
+                elem = elem.Next;
+            }
+            sb.Append("null");
+            return sb.ToString();
+        }
+
+
+
+    }
+
+
+
     public class Elem
     {
         public int Info { get; set; }
--- ConsoleApp26/ConsoleApp26/Program.cs
+++ ConsoleApp26/ConsoleApp26/Program.cs
@@ -7,8 +7,27 @@ using System.Threading.Tasks;
 namespace ConsoleApp26
 {
 
-    
+    class Student:IComparable
+    {
+        public int Id { get; set; }
+        public string Fio { get; set; }
 
+        public int CompareTo(object obj)
+        {
+            var st = obj as Student;
+            var fl = Id.CompareTo(st.Id);
+            if (fl == 0)
+                return Fio.CompareTo(st.Fio);
+            return fl;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Fio}";
+        }
+
+
+    }
 
 
     class Program
@@ -16,18 +35,31 @@ namespace ConsoleApp26
         static void Main(string[] args)
         {
 
-            MyList lst = new MyList();
+            
+            MyList<Student> lst = new MyList<Student>();
 
             Console.WriteLine(lst.Length);
 
-            for (int i = 0; i < 11; i++)
-                lst.AddLast(i);
+            lst.AddLast(new Student() { Id = 5, Fio = "Ivanov 1" });
+
+            lst.AddLast(new Student() { Id = 1, Fio = "Ivanov 2" });
+            lst.AddLast(new Student() { Id = 1, Fio = "Ivanov 3" });
+            lst.AddLast(new Student() { Id = 4, Fio = "Ivanov 4" });
+
+            //for (int i = 0; i < 10; i++)
+            //    lst.AddLast(i.ToString());
 
             Console.WriteLine(lst);
 
-            lst.DeleteIndex(5);
+            //lst.DeleteIndex(5);
+            //lst.AddLast("a");
+            //lst.AddLast("aaa");
+            //lst.AddLast("aac");
+            //lst.AddLast("aab");
 
-            Console.WriteLine(lst);
+            //Console.WriteLine(lst);
+
+            Console.WriteLine(lst.IsSorted());
 
         }
     }
