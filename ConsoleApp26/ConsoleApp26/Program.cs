f54de9f36983d22c6f0f7f47eb011b1059fde8b7
moudroff <moudroff>
2020-03-02 09:52:56 +0300

    Delegate Lambda

--- ConsoleApp26/ConsoleApp26/Program.cs
+++ ConsoleApp26/ConsoleApp26/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp26
         public int Id { get; set; }
         public string Fio { get; set; }
 
+        public int Age { get; set; }
         public int CompareTo(object obj)
         {
             var st = obj as Student;
@@ -23,7 +24,7 @@ namespace ConsoleApp26
 
         public override string ToString()
         {
-            return $"{Id} {Fio}";
+            return $"{Id} {Fio} {Age}";
         }
 
 
@@ -32,24 +33,63 @@ namespace ConsoleApp26
 
     class Program
     {
+        public static List<Student> lst = new List<Student>();
+        delegate bool MyComparer(Student s1, Student s2); 
+
+
+        static void Sort(MyComparer comp)
+        {
+            bool fl = true;
+            while (fl)
+            {
+                fl = false;
+                for (int i = 0; i < lst.Count - 1; i++)
+                {
+                    if (!comp.Invoke(lst[i], lst[i + 1]))
+                    {
+                        var st = lst[i];
+                        lst[i] = lst[i + 1];
+                        lst[i + 1] = st;
+                        fl = true;
+                    }
+                }
+            }
+
+        }
+
+
         static void Main(string[] args)
         {
 
+
+
+            //Console.WriteLine(lst.Length);
+
+            //lst.AddLast(new Student() { Id = 5, Fio = "Ivanov 1" });
+
+            //lst.AddLast(new Student() { Id = 1, Fio = "Ivanov 2" });
+            //lst.AddLast(new Student() { Id = 1, Fio = "Ivanov 3" });
+            //lst.AddLast(new Student() { Id = 4, Fio = "Ivanov 4" });
+
+            lst.Add(new Student() { Id = 5, Fio = "Ivanov 10", Age=24 });
+            lst.Add(new Student() { Id = 1, Fio = "Ivanov 2", Age = 15 });
+            lst.Add(new Student() { Id = 2, Fio = "Ivanov 3", Age = 30 });
+            lst.Add(new Student() { Id = 4, Fio = "Ivanov 4", Age = 18 });
             
-            MyList<Student> lst = new MyList<Student>();
+            for (int i = 0; i < lst.Count; i++)
+                Console.WriteLine(lst[i]);
 
-            Console.WriteLine(lst.Length);
+           
+            Console.WriteLine("***********************");
 
-            lst.AddLast(new Student() { Id = 5, Fio = "Ivanov 1" });
+            Sort((s1, s2) =>  s1.Fio.CompareTo(s2.Fio) < 0 );
 
-            lst.AddLast(new Student() { Id = 1, Fio = "Ivanov 2" });
-            lst.AddLast(new Student() { Id = 1, Fio = "Ivanov 3" });
-            lst.AddLast(new Student() { Id = 4, Fio = "Ivanov 4" });
 
-            //for (int i = 0; i < 10; i++)
-            //    lst.AddLast(i.ToString());
+            for (int i = 0; i < lst.Count; i++)
+                Console.WriteLine(lst[i]);
 
-            Console.WriteLine(lst);
+
+            //Console.WriteLine(lst);
 
             //lst.DeleteIndex(5);
             //lst.AddLast("a");
@@ -59,7 +99,7 @@ namespace ConsoleApp26
 
             //Console.WriteLine(lst);
 
-            Console.WriteLine(lst.IsSorted());
+            //Console.WriteLine(lst.IsSorted());
 
         }
     }
---
+++ DelegApp/DelegApp/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegApp
+{
+    class ArOper
+    {
+        public static int Sum(int a,int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mult(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static void Hello1()
+        {
+            Console.WriteLine("Hello 1");
+        }
+
+        public static void Hello2()
+        {
+            Console.WriteLine("Hello 2");
+        }
+
+
+    }
+
+
+
+    class Program
+    {
+        public delegate int Oper(int x, int y);
+        delegate void Hello();
+
+        static void Main(string[] args)
+        {
+            ArOper a = new ArOper();
+
+            Oper op = new Oper(ArOper.Sum);
+            
+            Console.WriteLine(op(5, 3));
+
+            //op = ArOper.Sub;
+            //Console.WriteLine(op(5, 3));
+
+            //op = a.Mult;
+            //Console.WriteLine(op(5, 3));
+
+            //op = ArOper.Div;
+            //Console.WriteLine(op(5, 3));
+
+            op = (x,y) => x - y;
+
+            Console.WriteLine(op(5, 3));
+
+            op = (x,y) => { return x * y;};
+
+            Console.WriteLine(op(5, 3));
+            
+            op = (x, y) => x / y;
+
+            Console.WriteLine(op(5, 3));
+
+
+            Hello he = ArOper.Hello1;
+            he += ArOper.Hello2;
+            he += delegate ()
+            {
+                Console.WriteLine("Hello from anonym method");
+            };
+
+            he += () =>
+            {
+                Console.WriteLine("Hello from lambda");
+            };
+            
+            //he -= ArOper.Hello2;
+            //he -= ArOper.Hello1;
+
+            he.Invoke();
+
+
+        }
+
+        public static int MyMethod(Oper op)
+        {
+            
+            return 0;
+        }
+
+
+    }
+}
