e54931da49de4c971275afec8b9136d325fd74d4
moudroff <moudroff>
2020-03-11 09:41:33 +0300

    linq groupby

---
+++ LinqGroupBy/LinqGroupBy/Fitness.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqGroupBy
+{
+    public  class Fitness
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Duration { get; set; }
+        public int ClientId { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Year} {Month} {Duration} {ClientId}";
+        }
+
+
+        public static List<Fitness> GenerateData()
+        {
+            var lst = new List<Fitness>();
+            lst.Add(new Fitness() { Year = 2020, Month = 1, ClientId = 101, Duration = 20 });
+            lst.Add(new Fitness() { Year = 2020, Month = 1, ClientId = 102, Duration = 25 });
+            lst.Add(new Fitness() { Year = 2020, Month = 2, ClientId = 103, Duration = 30 });
+            lst.Add(new Fitness() { Year = 2020, Month = 2, ClientId = 101, Duration = 40 });
+            lst.Add(new Fitness() { Year = 2020, Month = 3, ClientId = 102, Duration = 15 });
+            lst.Add(new Fitness() { Year = 2019, Month = 1, ClientId = 103, Duration = 22 });
+            lst.Add(new Fitness() { Year = 2019, Month = 2, ClientId = 101, Duration = 10 });
+            lst.Add(new Fitness() { Year = 2019, Month = 2, ClientId = 102, Duration = 50 });
+            lst.Add(new Fitness() { Year = 2018, Month = 1, ClientId = 101, Duration = 120 });
+
+            return lst;
+        }
+
+
+    }
+}
---
+++ LinqGroupBy/LinqGroupBy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqGroupBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lst = Fitness.GenerateData();
+
+
+            var gr = lst.GroupBy(f => f.Year).Select(g =>
+                    new
+                    {
+                        Year = g.Key,
+                        Sum = g.Sum(f => f.Duration)
+                    });
+
+            var res = gr.Where(g => g.Sum == gr.Max(k => k.Sum))
+                    .OrderBy(r=> r.Year).Take(1);
+
+
+
+            foreach (var g in res)
+            {
+                Console.WriteLine(g);
+                //Console.WriteLine(g.Key);
+                //foreach (var f in g)
+                //    Console.WriteLine($"      {f}");
+
+            }
+
+
+
+
+        }
+    }
+}
