15496b623a2e09a85296ae4ffa89cbd575b9f80a
moudroff <moudroff>
2020-05-08 15:07:15 +0300

    Priority Queue

--- PriorityQueue/PriorityQueue/Program.cs
+++ PriorityQueue/PriorityQueue/Program.cs
@@ -8,58 +8,80 @@ namespace PriorityQueue
 {
     public class Elem
     {
-        public int Key { get; set; }
+        public int Priority { get; set; }
         public string Value { get; set; }
 
         public override string ToString()
         {
-            return $"{Key} -- {Value}";
+            return $"{Priority} -- {Value}";
         }
-    }
 
+    }
+   
     public class PriorityQueue
     {
+        private List<Elem> lst = new List<Elem>();
 
-        private List<Elem> elems = new List<Elem>();
+        private int HeapSize => lst.Count;
 
-        public bool IsEmpty => elems.Count == 0;
+        public bool IsEmpty => HeapSize == 0;
 
-        public void Insert(Elem elem)
+        public void Insert(Elem el)
         {
-            elems.Add(elem);
-            int i = elems.Count - 1;
+            lst.Add(el);
+
+            int i = HeapSize - 1;
             int parent = (i - 1) / 2;
 
-            while (i > 0 && elems[parent].Key < elems[i].Key)
+            while (i > 0 && 
+                lst[parent].Priority < lst[i].Priority)
             {
-                var temp = elems[i];
-                elems[i] = elems[parent];
-                elems[parent] = temp;
+                var temp = lst[i];
+                lst[i] = lst[parent];
+                lst[parent] = temp;
 
                 i = parent;
                 parent = (i - 1) / 2;
             }
         }
 
-        private void Heapify(int i)
+        public Elem Extract()
+        {
+            if (IsEmpty)
+                throw new IndexOutOfRangeException("Queue is empty!");
+
+            var result = new Elem()
+            {
+                Priority = lst[0].Priority,
+                Value = lst[0].Value
+            };
+            lst[0] = lst[HeapSize - 1];
+            lst.RemoveAt(HeapSize - 1);
+            Heapify(0);
+
+            return result;
+        }       
+
+        public void Heapify(int i)
         {
             int leftChild;
             int rightChild;
             int largestChild;
-            while (true)
+
+            while(true)
             {
                 leftChild = 2 * i + 1;
                 rightChild = 2 * i + 2;
                 largestChild = i;
 
-                if (leftChild < elems.Count &&
-                    elems[leftChild].Key > elems[largestChild].Key)
+                if (leftChild < HeapSize && 
+                    lst[leftChild].Priority > lst[largestChild].Priority)
                 {
                     largestChild = leftChild;
                 }
 
-                if (rightChild < elems.Count &&
-                    elems[rightChild].Key > elems[largestChild].Key)
+                if (rightChild < HeapSize && 
+                    lst[rightChild].Priority > lst[largestChild].Priority)
                 {
                     largestChild = rightChild;
                 }
@@ -69,72 +91,64 @@ namespace PriorityQueue
                     break;
                 }
 
-                var temp = elems[i];
-                elems[i] = elems[largestChild];
-                elems[largestChild] = temp;
+                var temp = lst[i];
+                lst[i] = lst[largestChild];
+                lst[largestChild] = temp;
+                
                 i = largestChild;
             }
-
-        }
-
-
-        public string Extract()
-        {
-            var extrValue = elems[0].Value;
-
-            var temp = elems[0];
-            elems[0] = elems[elems.Count-1];
-            elems[elems.Count-1] = temp;
-            elems.RemoveAt(elems.Count - 1);
-
-            Heapify(0);
-            return extrValue;
         }
 
         public override string ToString()
         {
-            StringBuilder sb = new StringBuilder();
-            foreach (var el in elems)
-                sb.Append($"{el}, ");
+            int i = 1,k=0;
+            var sb = new StringBuilder();
+            foreach(var el in lst)
+            {
+                sb.Append($"{el},  ");
+                k++;
+                if(k==i)
+                {
+                    sb.AppendLine();
+                    i *= 2;
+                    k = 0;
+                }
+            }
             return sb.ToString();
         }
-
 
     }
 
 
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             var pr = new PriorityQueue();
-            pr.Insert(new Elem() { Key = 10, Value = "A" });
-            pr.Insert(new Elem() { Key = 4, Value = "B" }); 
-            pr.Insert(new Elem() { Key = 16, Value = "C" });
-            pr.Insert(new Elem() { Key = 9, Value = "D" });
-            pr.Insert(new Elem() { Key = 8, Value = "E" });
-            pr.Insert(new Elem() { Key = 35, Value = "F" });
+            Console.WriteLine(pr.Extract().Value);
 
-            Console.WriteLine(pr);
-            if(! pr.IsEmpty)
-              Console.WriteLine(pr.Extract());
-            Console.WriteLine(pr);
-
-            if (!pr.IsEmpty)
-                Console.WriteLine(pr.Extract());
+            pr.Insert(new Elem() { Priority = 10, Value = "A" });
+            pr.Insert(new Elem() { Priority = 6, Value = "B" });
+            pr.Insert(new Elem() { Priority = 17, Value = "C" });
+            pr.Insert(new Elem() { Priority = 150, Value = "D" });
+            pr.Insert(new Elem() { Priority = 62, Value = "E" });
+            pr.Insert(new Elem() { Priority = 3, Value = "F" });
+            pr.Insert(new Elem() { Priority = 15, Value = "G" });
+            pr.Insert(new Elem() { Priority = 36, Value = "H" });
+            pr.Insert(new Elem() { Priority = 1, Value = "I" });
 
             Console.WriteLine(pr);
 
-            if (!pr.IsEmpty)
-                Console.WriteLine(pr.Extract());
-
+            Console.WriteLine(pr.Extract().Value);
+            Console.WriteLine(pr);
+            
+            Console.WriteLine(pr.Extract().Value);
+            Console.WriteLine(pr);
+            pr.Insert(new Elem() { Priority = 16, Value = "K" });
             Console.WriteLine(pr);
 
-
-
-
-
-
+            Console.WriteLine(pr.Extract().Value);
+            Console.WriteLine(pr);
         }
     }
 }
