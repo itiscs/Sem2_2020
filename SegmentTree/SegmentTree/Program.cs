56324d4b1c3c9d4893ae6161aca252f76013521b
moudroff <moudroff>
2020-05-15 13:48:18 +0300

    Segment Tree 912

--- SegmentTree/SegmentTree/Program.cs
+++ SegmentTree/SegmentTree/Program.cs
@@ -9,19 +9,22 @@ namespace SegmentTree
     class SegmentTree
     {
         public int Size { get; private set; }
-        private int[] mas;
-        public SegmentTree(int[] a, int n)
+        
+        private int[] tree;  //дерево отрезков. в вершинах хранятся суммы
+
+        public SegmentTree(int[] a, int size)
         {
-            Size = n;
-            mas = new int[4 * n];
-            Build_tree(a, 1, 0, n - 1);
+            Size = size;
+            tree = new int[Size * 4];
+            Build_tree(a, 1, 0, Size - 1);
         }
 
         private void Build_tree(int[] a, int v, int tl, int tr)
         {
             if (tl == tr)
             {
-                mas[v] = a[tl];    //сумма отрезка из одного элемента равна этому элементу
+                tree[v] = a[tl];
+                //сумма отрезка из одного элемента равна этому элементу
             }
             else
             {
@@ -30,33 +33,41 @@ namespace SegmentTree
                 int tm = (tl + tr) / 2;
                 Build_tree(a, v * 2,     tl, tm);
                 Build_tree(a, v * 2 + 1, tm + 1, tr);
-                mas[v] = mas[v * 2] + mas[v * 2 + 1];
-                //mas[v] = mas[v * 2] > mas[v * 2 + 1]?mas[v*2]:mas[v*2+1];
+
+                tree[v] = tree[v * 2] + tree[v * 2 + 1];
+                /*tree[v] = tree[v * 2] > tree[v * 2 + 1]
+                    ? tree[v*2] : tree[v*2+1];*/
             }
         }
 
-        public int Get_Sum(int l, int r)
+        public int Get_sum(int l, int r)
         {
             return Get_sum(l, r, 1, 1, Size);
         }
-
         private int Get_sum(int l, int r, int v, int tl, int tr)
         {
             //вариант 1
-            if (l <= tl && tr <= r) // Текущий отрезок полностью входит в запрос (l≤tl;tr≤r).
+            //Текущий отрезок полностью входит в запрос 
+            if (l <= tl && tr <= r)
             {
-                return mas[v];
+                return tree[v];
             }
 
             //вариант 2
-            if (tr < l || r < tl)  //Текущий отрезок полностью не входит в запрос (tr<l или l<tr).
+            //Текущий отрезок полностью не входит в запрос
+            if (tr < l || r < tl)
             {
                 return 0;
             }
 
             //вариант 3
-            //Текущий отрезок частично входит в запрос.
+            //Текущий отрезок частично входит в запрос
             int tm = (tl + tr) / 2;
+            
+            /*var ml = Get_sum(l, r, v * 2, tl, tm);
+            var mr = Get_sum(l, r, v * 2 + 1, tm + 1, tr);
+            return (ml > mr)? ml : mr;
+            */
             return Get_sum(l, r, v * 2, tl, tm)
                  + Get_sum(l, r, v * 2 + 1, tm + 1, tr);
         }
@@ -65,19 +76,12 @@ namespace SegmentTree
         {
             Update(idx, val, 1, 1, Size);
         }
-
-        //TODO
-        public void Update(int idxl, int idxr, int[] mval)
-        {
-            throw new NotImplementedException();
-        }
-
         private void Update(int idx, int val, int v, int tl, int tr)
         {
             //вариант 1
             if (idx <= tl && tr <= idx)
             {    //То же, что и idx == tl == tr
-                mas[v] = val;
+                tree[v] = val;
                 return;
             }
 
@@ -91,19 +95,33 @@ namespace SegmentTree
             int tm = (tl + tr) / 2;
             Update(idx, val, v * 2, tl, tm);
             Update(idx, val, v * 2 + 1, tm + 1, tr);
-            mas[v] = mas[v * 2] + mas[v * 2 + 1];
+
+            tree[v] = tree[v * 2] + tree[v * 2 + 1];
+            /*tree[v] = tree[v * 2] > tree[v * 2 + 1]
+                  ? tree[v * 2] : tree[v * 2 + 1];
+                  */
         }
+
 
 
 
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
-            for (int i = 1; i < 4 * Size; i++)
-                sb.Append($"{mas[i]} ");
+            int k = 2;
+            for(int i=1; i < 4*Size; i++)
+            {
+                if (i == k)
+                {
+                    sb.AppendLine();
+                    k *= 2;
+                }
+                sb.Append($"{tree[i]} ");
+                
+            }
+
             return sb.ToString();
         }
-
 
 
     }
@@ -114,41 +132,25 @@ namespace SegmentTree
         static void Main(string[] args)
         {
             int n = 1000;
-            int[] a = new int[n];
-            for(int i =0; i<n; i++)
+            int[] mas = new int[n];
+            for(int i=0; i<n; i++)
             {
-                a[i] = i + 1;
+                mas[i] = i;
             }
-            SegmentTree st = new SegmentTree(a, n);
+            SegmentTree st = new SegmentTree(mas,n);
 
+            
+            Console.WriteLine($"Sum = {st.Get_sum(901,903)}");
+
+            st.Update(901, 10);
+            st.Update(902, 20);
+            st.Update(903, 30);
             //Console.WriteLine(st);
 
-            Console.WriteLine(st.Get_Sum(600, 602));
-            st.Update(600, 0);
-            st.Update(601, 0);
-            st.Update(602, 0);
-            Console.WriteLine(st.Get_Sum(600, 603));
+            Console.WriteLine($"Sum = {st.Get_sum(901, 903)}");
 
 
         }
     }
 }
 
-
-
-/*
- 
-//Запрос модификации
-//idx - индекс элемента, val - новое значение
-//v - номер текущей вершины; tl, tr - границы соответствующего отрезка
-int main()
-{
-    //Ввод массива...
-
-        //параметры корня дерева.
-                                //все запросы должны вызываться для этих параметров.
-
-    // get_sum(l, r, 1, 0, n - 1) 
-    // update(idx, val, 1, 0, n - 1);
-}
-*/
